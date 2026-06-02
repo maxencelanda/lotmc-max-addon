8a34e5145a69b7cb6e0c42e0b4bf5637b0e83b52
maxencelanda <moukasland@gmail.com>
2026-06-02 21:58:04 +0200

    fix: replace raw Thread with MC tick scheduler and add sanity damage in MentalDisruptionAbility

--- src/main/java/net/swimmingtuna/lotmc/marauder/item/MentalDisruptionAbility.java
+++ src/main/java/net/swimmingtuna/lotmc/marauder/item/MentalDisruptionAbility.java
@@ -15,6 +15,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.swimmingtuna.lotm.capabilities.sanity_data.SanityUtil;
 import net.swimmingtuna.lotm.init.BeyonderClassInit;
 import net.swimmingtuna.lotm.item.BeyonderAbilities.SimpleAbilityItem;
 
@@ -22,7 +23,7 @@ import java.util.List;
 import java.util.Random;
 
 public class MentalDisruptionAbility extends SimpleAbilityItem {
-    private static final int RANGE = 10;
+    private static final int RANGE = 15;
     private static final SoundEvent[] HALLUCINATION_SOUNDS = {
             SoundEvents.CREEPER_PRIMED,
             SoundEvents.WARDEN_HEARTBEAT,
@@ -31,7 +32,7 @@ public class MentalDisruptionAbility extends SimpleAbilityItem {
     };
 
     public MentalDisruptionAbility(Properties properties) {
-        super(properties, () -> BeyonderClassInit.MARAUDER.get(), 8, 60, 300);
+        super(properties, () -> BeyonderClassInit.MARAUDER.get(), 8, 100, 1200);
     }
 
     @Override
@@ -43,7 +44,7 @@ public class MentalDisruptionAbility extends SimpleAbilityItem {
 
         if (user.distanceToSqr(target) >= RANGE * RANGE) {
             thief.displayClientMessage(
-                    Component.literal("Target is too far!").withStyle(ChatFormatting.RED),
+                    Component.translatable("message.lotmc_marauder.target_too_far").withStyle(ChatFormatting.RED),
                     false
             );
             return InteractionResult.FAIL;
@@ -53,16 +54,16 @@ public class MentalDisruptionAbility extends SimpleAbilityItem {
         Random random = new Random();
         int soundCount = 1 + random.nextInt(3);
 
+        long baseDelay = 4000L + random.nextInt(1000);
+        long interval = 2000L + random.nextInt(1000);
+
         for (int i = 0; i < soundCount; i++) {
             SoundEvent sound = HALLUCINATION_SOUNDS[random.nextInt(HALLUCINATION_SOUNDS.length)];
-            long delayMs = (long) i * (1000L + random.nextInt(1000));
-
-            if (delayMs == 0) {
-                sendHallucination(serverVictim, sound);
-            } else {
-                scheduleHallucination(serverVictim, sound, delayMs);
-            }
+            long delayMs = baseDelay + (long) i * interval;
+            scheduleHallucination(serverVictim, sound, delayMs);
         }
+
+        SanityUtil.decreaseSanity(victim, 3);
 
         this.addCooldown(thief);
         this.useSpirituality(thief);
@@ -80,27 +81,23 @@ public class MentalDisruptionAbility extends SimpleAbilityItem {
     }
 
     private static void scheduleHallucination(ServerPlayer victim, SoundEvent sound, long delayMs) {
-        new Thread(() -> {
-            try {
-                Thread.sleep(delayMs);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                return;
-            }
-            victim.connection.send(new ClientboundSoundPacket(
-                    Holder.direct(sound),
-                    SoundSource.AMBIENT,
-                    victim.getX(), victim.getY(), victim.getZ(),
-                    1.0f, 1.0f,
-                    victim.getRandom().nextLong()
-            ));
-        }).start();
+        // Use the server thread tick scheduler instead of a raw Thread
+        int delayTicks = Math.max(1, (int) (delayMs / 50));
+        tickDownHallucination(victim, sound, delayTicks);
+    }
+
+    private static void tickDownHallucination(ServerPlayer victim, SoundEvent sound, int ticksLeft) {
+        if (ticksLeft <= 0) {
+            sendHallucination(victim, sound);
+        } else {
+            victim.getServer().execute(() -> tickDownHallucination(victim, sound, ticksLeft - 1));
+        }
     }
 
     @Override
     public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, level, tooltip, flag);
-        tooltip.add(Component.literal("Plays terrifying hallucination sounds in the target's ears").withStyle(ChatFormatting.GRAY));
-        tooltip.add(Component.literal("Range: " + RANGE + " blocks").withStyle(ChatFormatting.GRAY));
+        tooltip.add(Component.translatable("tooltip.lotmc_marauder.mental_disruption.desc").withStyle(ChatFormatting.GRAY));
+        tooltip.add(Component.translatable("tooltip.lotmc_marauder.mental_disruption.range", RANGE).withStyle(ChatFormatting.GRAY));
     }
 }
